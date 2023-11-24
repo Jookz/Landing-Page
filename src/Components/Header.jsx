2111b0774821aa5fce1b2dd52e3ceab9f17180a4
Jookz <j.chatterton456@gmail.com>
2023-11-24 13:16:34 +0000

    minor changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const feather = require("feather-icons");
-import { Home } from "react-icons/fi";
+import HomeLogo from "../../home-logo.svg";
 
 export default function Header() {
-  console.log(feather.icons.x);
   return (
     <div>
       <Link to="/">
-        <img
-          className="h-8 float-right m-4"
-          src="../home-icon.png"
-          alt="Home Icon"
-        />
+        <img className="h-8 float-right m-4" src={HomeLogo} alt="Home Icon" />
       </Link>
       <h1 className="text-green-900 text-6xl">Jonathan Chatterton</h1>
       <h3 className="text-green-900 text-6xl">Junior Software Developer</h3>
